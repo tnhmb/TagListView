ab3f4b00d8eb8fed61117d09eb9a23d4194cd7bd
tnhmb <tnhmb@hotmail.com>
2018-11-23 23:03:31 +0800

    Add files via upload

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -239,7 +239,7 @@ open class TagListView: UIView {
             tagView.frame.size = tagView.intrinsicContentSize
             tagViewHeight = tagView.frame.height
             
-            if currentRowTagCount == 0 || currentRowWidth + tagView.frame.width > frame.width || currentRowTagCount < 4{
+            if currentRowTagCount == 0 || currentRowTagCount > 3{
                 currentRow += 1
                 currentRowWidth = 0
                 currentRowTagCount = 0
